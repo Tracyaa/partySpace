8e3d2dd0996f181e5ede25b1ffc44ed6ff77921c
Nate <github email address>
2019-03-05 10:12:11 -0500

    relationships made

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
-  has_many :venue_bookings, through: :bookings, source: :venue
+  has_many :venues
+  has_many :bookings
+  # has_many :venue_bookings, through: :bookings, source: :venues
+  has_many :venue_bookings, :through => :bookings, :source => :venue
+  # 'has_many :venue_bookings, :through => :bookings, :source => <name>'. Is it one of user or venue?
+  has_many :favorites
   has_many :venue_favorites, through: :favorites, source: :venue
+
 end
